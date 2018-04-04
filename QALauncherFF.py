4467f77c7b9f7dce5f201bfddd3bbad9aae82bbd
agagnon227 <37886626+agagnon227@users.noreply.github.com>
2018-04-03 21:28:11 -0400

    Update QALauncherFF.py

--- QALauncherFF.py
+++ QALauncherFF.py
@@ -7,9 +7,7 @@ import time, getpass
 myAccount = input('Account: ')
 myPass = getpass.getpass('Password: ')
 browser = webdriver.Firefox()
-sites = ['https://outlook.office.com/', 'https://hipchat.com/sign_in/',
-        'https://jira.transparent.com/', 'https://confluence.transparent.com',
-        'https://screencast-o-matic.com/screen_recorder/']
+sites = ['site1', 'site2', 'site3', 'site4']
 
 def delay():
     return time.sleep(2)
@@ -27,36 +25,39 @@ for tabs in range(len(sites)-1):
 
 windowSwitch(0)
 browser.get(sites[0])
-officeEmail = idIt('i0116')
-officeEmail.send_keys(myAccount + '@transparent.com')
-idIt('idSIButton9').click()
+Email0 = idIt('email')
+Email0.send_keys(myAccount + '@test.com')
+idIt('test').click()
 delay()
-officePass = idIt('i0118')
-officePass.send_keys(myPass)
+Pass0 = idIt('password')
+Pass0.send_keys(myPass)
 idIt('idSIButton9').click()
 delay()
 idIt('idBtn_Back').click()
 
 windowSwitch(1)
 browser.get(sites[1])
-hipEmail = idIt('email')
-hipEmail.send_keys(myAccount + '@transparent.com')
-hipEmail.submit()
+Email1 = idIt('email')
+Email1.send_keys(myAccount + '@test.com')
+Email1.submit()
 delay()
-hipPass = idIt('password')
-hipPass.send_keys(myPass)
-hipPass.submit()
+Pass1 = idIt('password')
+Pass1.send_keys(myPass)
+Pass1.submit()
 delay()
-chat = browser.find_element_by_link_text('Launch the web app')
+chat = browser.find_element_by_link_text('Launch this link')
 chat.click()
 
 windowSwitch(2)
 browser.get(sites[2])
-jiraEmail = idIt('login-form-username')
-jiraEmail.send_keys(myAccount)
-jiraPass = idIt('login-form-password')
-jiraPass.send_keys(myPass)
-jiraPass.submit()
+Email2 = idIt('email')
+Email2.send_keys(myAccount)
+Pass2 = idIt('password')
+Pass2.send_keys(myPass)
+Pass2.submit()
+
+windowSwitch(3)
+browser.get(sites[3])
 
 windowSwitch(4)
 browser.get(sites[4])
